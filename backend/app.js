f8bf0c982a6555c70f94876925d241eb3a642aa3
mohsinshah12309 <alishahmohsin938@gmail.com>
2025-12-07 00:57:33 +0500

    Fix ReferenceError: Restore app initialization in backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,4 @@
 // backend/app.js - CLEAN VERSION WITH DEBUG LOGS
-
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
